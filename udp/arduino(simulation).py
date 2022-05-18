81e4a809172c37dc410cbcd5c08cd5d2b086f99a
HOLLOWM <2933370771@qq.com>
2022-05-18 11:13:10 +0800

    update-2022-05-18

--- udp/arduino(simulation).py
+++ udp/arduino(simulation).py
@@ -14,7 +14,8 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 PORT = 7777
 
-server_address = ("192.168.3.180", PORT)  # 接收方 服务器的ip地址和端口号
+# server_address = ("192.168.3.180", PORT)  # 接收方 服务器的ip地址和端口号
+server_address = ("127.0.0.1", PORT)  # 接收方 服务器的ip地址和端口号
 
 sleeptime = 0.2
 
